c4f6df6ab909f0991a509a63637f6253f06d5a2c
PARKBYEONGKEN <85853147+PARKBYEONGKEN@users.noreply.github.com>
2021-08-10 09:18:17 +0900

    12

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
         <el-container>
           <el-header>
             <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-              <el-menu-item index="1" v-bind:class="on">홈으로</el-menu-item>
-              <el-menu-item index="2" v-bind:class="on1">게시판목록</el-menu-item>
-              <el-menu-item index="3" v-bind:class="on2">게시판글쓰기</el-menu-item>
+              <el-menu-item index="1">홈으로</el-menu-item>
+              <el-menu-item index="2" >게시판목록</el-menu-item>
+              <el-menu-item index="3" >게시판글쓰기</el-menu-item>
             </el-menu>
           </el-header>
 
@@ -28,30 +28,22 @@
 export default {
   data(){
     return{
-    on:'on',
-    on1:'',
-    on2:''
+ 
     }
   },
   methods:{
     handleSelect(val){
       if(val === '1'){
         this.$router.push({path:'/'})
-        this.on ="red"
-        this.on1="on"
-        this.on2="on"
+       
       }
       if(val === '2'){
         this.$router.push({path:'/menu1'})
-        this.on ="on"
-        this.on1="red"
-        this.on2="on"
+        
       }
       if(val === '3'){
         this.$router.push({path:'/menu2'})
-         this.on ="on"
-        this.on1="on"
-        this.on2="red2"
+    
       }      
       console.log(val);
     }
@@ -63,56 +55,23 @@ export default {
   *{
     margin:0px;
     padding:0px;
+    background: #1B1D1F!important;
   }
-.on{
-  color:red;
-  
-}
-.red{
-  color: blue!important;
-  font-weight: bold;
-}
-.red2:hover{
-  color: blue !important;
-  font-weight: bold;
-}
-.red2:active{
-  color: blue!important;
-  font-weight: bold;
-}
 
-.m1{
-   padding:10px;
-   text-decoration: none;
-   color: blue;
-  }
-  .m1:hover{
-    color:red;
-  }
- .m1:active{
-   background: rgba(255,255,0,0.1);
-   background: 400px;
- 
- }
- .logo{
-   width:80px;
-   
- }
- .logowrap{
-   text-align: center;
- }
+
 
 
   /* ----------element ------------------- */
   .el-menu-demo{
-    background: #000!important;
+    
+    border: none!important;
   }
   
 
   .el-header, .el-footer {
     /*background-color: #B3C0D1;*/
-    color: #333;
-    background: #000;
+    color: #1B1D1F;
+    background: #1B1D1F;
     text-align: center;
     line-height: 60px;
   }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,89 +1,26 @@
 <template>
-  <div class="hello">
-     <h2>
-  <span>프론트엔드 개발자</span>  
-  <span class="light">박병근</span> 
-  <span> even in</span> 
-  <span>the darkest</span>  
-  <span>places</span>
-</h2>
-  </div>
+<div id="wrap">
+   
+  
+  
+  </div>  
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  methods:{
+  
+  },
   props: {
-    msg: String
+   
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.hello{
-  height: 937px;
-  background: #000;
- 
-}
-body {
-  background: #000;
-  padding: 2rem;
-}
-
-h2 {
-  font-family: 'Arial';
-  color: #fff;
-  text-transform: uppercase;
-  font-weight: bold;
-  font-size: 3rem;
-  line-height: 0.75;
-}
-
-span {
-  display: block;
-  text-align: left;
-  clear: both;
-}
-
-span:not(.light) {
-  opacity: 0;
-  animation: flashText .6s ease-out alternate infinite;
-}
-
-span.light {
-  position: relative;
-  display: inline-block;
-  float:left;
-  
-}
-  span.light:before {
-    position: absolute;
-    left: 0;
-    top: -10%;
-    width: 100%;
-    height: 120%;
-    background: #fff;
-    filter: blur(10px);
-    content: "";
-    opacity: 0;
-    animation: flash .6s ease-out alternate infinite;
-   
-  
-}
-
-@keyframes flash{
-  to {
-    opacity: 1;
+  #wrap{
+    background: #1B1D1F;
+    height: 937px;
   }
-}
-
-@keyframes flashText {
-  to {
-    opacity: 0.3;
-  }
-}
-
-
-
 </style>
